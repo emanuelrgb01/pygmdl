8fc7a8a27b5196636820eb69910f9e043ac8043e
Emanuel Reinaldo <80110729+emanuelrgb01@users.noreply.github.com>
2025-08-12 23:58:59 -0300

    Add Freitas references on pygmdl lib main interface

--- src/pygmdl/__init__.py
+++ src/pygmdl/__init__.py
@@ -4,6 +4,13 @@ pygmdl: A Python implementation of the Gaussian Mixture Descriptors Learner (GMD
 This library provides an online classifier based on the Minimum Description
 Length (MDL) principle, using Online Kernel Density Estimators
 for density modeling.
+
+FREITAS, B. L. de. Aprendiz de Descritores de Mistura Gaussiana. 2017. 90 f.
+Dissertacao (Mestrado em Ciencia da Computacao) — Universidade Federal de Sao
+Carlos, Sorocaba, 2017.
+
+Paper: https://repositorio.ufscar.br/items/09ccfb95-d5dd-47ae-a364-af8641de6e2d
+C++ code from Freitas: https://github.com/brenolf/gmdl/tree/master
 """
 
 from .core import GMDL
